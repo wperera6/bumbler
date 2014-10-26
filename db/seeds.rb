1519c20a1f06a3c6bef51e35d6f56ee8e622904e
Waruna Perera <wperera6@waru.home>
2014-10-26 12:44:18 -0400

    Seeds file, post added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+post1 = Post.new
+post1.title = "Bagel, Yummm" 
+post1.kind = "image"
+post1.content = "http://tahinitoo.files.wordpress.com/2012/10/bagels-02.jpg?w=584&h=390"
+post1.save
+
+2.times do 
+	post1.hearts.create
+end	
